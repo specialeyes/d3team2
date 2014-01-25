23a7d95320cd53b74180c235e502bc55592bbcae
Brandon Duarte <brandonduarte03@gmail.com>
2014-01-25 18:04:59 -0500

    Data points change based on X and Y labels. Added labels to Axes

--- js/main.js
+++ js/main.js
@@ -9,31 +9,31 @@ function init(xAxisLabel, yAxisLabel){
 	var w = 1024;
 	var h = 540;
 	var padding = 40;
-	var dataset = [];
+	dataset = [];
 	var colors = [d3.rgb("#17becf"), d3.rgb("#e6550d"), d3.rgb("#2ca02c")];
 
 	//Selecting svg element
-	var svg = d3.select("#vis")
+	svg = d3.select("#vis")
 		.append("svg")
 		.attr("width", w)
 		.attr("height", h);
 
 	//Scale functions
-	var xScale = d3.scale.linear()
+	xScale = d3.scale.linear()
 	    .domain([5, 7])
 	    .range([padding, w-padding])
 	    .nice();
 
-    var yScale = d3.scale.linear()
+    yScale = d3.scale.linear()
 	    .domain([2.5, 4.5])
 	    .range([h-padding, padding])
 	    .nice();
 
 	//Axes
-	var xAxis = d3.svg.axis()
+	xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient("bottom");
-	var yAxis = d3.svg.axis()
+	yAxis = d3.svg.axis()
 		.scale(yScale)
 		.orient("left");
 
@@ -71,12 +71,24 @@ function init(xAxisLabel, yAxisLabel){
 	svg.append("g")
 		.attr("class", "axis")
 		.attr("transform", "translate(0," + (h - padding) + ")")
-		.call(xAxis);
+		.call(xAxis)
+		.append("text")
+		.attr("class", "xlabel")
+		.attr("x", w)
+		.attr("y", -6)
+		.style("text-anchor", "end")
+		.text(xAxisLabel + " (cm)");
 	svg.append("g")
 		.attr("class", "axis")
 		.attr("transform", "translate(" + padding + ", 0)")
-		.call(yAxis);
-
+		.call(yAxis)
+		.append("text")
+		.attr("class", "ylabel")
+		.attr("dy", ".75em" )
+		.attr("y", 6)
+		.attr("transform", "rotate(-90)")
+		.style("text-anchor", "end")
+		.text(yAxisLabel + " (cm)");
 }
 
 /**
@@ -86,6 +98,17 @@ x axis changes. It is passed the variable name that has been selected, such as
 "compactness". Populate this function to update the scatterplot accordingly.
 **/
 function onXAxisChange(value){
+
+	var circle = svg.selectAll("circle")
+			.data(dataset);
+			
+	circle.enter().append("circle")
+			.attr("r", 5);
+			
+	circle.attr("cx", function(d){return xScale(+d[value])});
+	
+	circle.exit().remove();
+	
 
 }
 
@@ -98,6 +121,16 @@ y axis changes. It is passed the variable name that has been selected, such as
 accordingly.
 **/
 function onYAxisChange(value){
+
+	var circle = svg.selectAll("circle")
+			.data(dataset);
+			
+	circle.enter().append("circle")
+			.attr("r", 5);
+			
+	circle.attr("cy", function(d){return yScale(+d[value])});
+	
+	circle.exit().remove();
 
 }
 
